cf7032bf044ca840c6e9155fc3c9a5dac56a0694
Greg <gregcudzich@gmail.com>
2021-10-10 15:27:25 -0500

    removed function fibonacci since it blocked the styles from the css file from being incorporated, added simple styles.css that just stylizes the table for fibonacci.

--- week07/fibonacciLogic.js
+++ week07/fibonacciLogic.js
@@ -1,22 +1,15 @@
-function fibonacciSequence() {
-    var userNum = prompt("Enter the number in which you would like the fibonacci sequence to go up to.")
-    var start = 1;
-    var finalNum = 0;
-    var tempNum;
-    //added counter so output would show F0, F1, F2...
-    var counter = 0;
-
-    while(userNum > 0) {
-        tempNum = start;
-        start = start + finalNum;
-        finalNum = tempNum;
-        userNum--;
-        document.write("<p>F" + counter++ + ": " + finalNum + "</p>");
-    }
-
+var userNum = prompt("Enter the number in which you would like the fibonacci sequence to go up to.")
+var start = 1;
+var finalNum = 0;
+var tempNum;
+//added counter so output would show F0, F1, F2...
+var counter = 0;
+document.write("<table>");
+document.write("<tr><th>Number</th><th>Fibonacci Number</th></tr>");
+while (userNum > 0) {
+    tempNum = start;
+    start = start + finalNum;
+    finalNum = tempNum;
+    userNum--;
+    document.write("<tr><td>" + "F" + counter++ + "</td><td>" + finalNum + "</td></tr>");
 }
-
-// This is so the function triggers once the HTML document has loaded
-window.onload = function() {
-    fibonacciSequence();
-  };
