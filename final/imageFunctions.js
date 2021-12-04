1f4e883c9f78268773ff7e2387e5de4cfdd1e076
Greg <gregcudzich@gmail.com>
2021-12-04 14:51:30 -0600

    fixed issue where on click search button would load initial content instead of content searched for, added on scroll event and on click event to body of web page which updates the offset and loads new images, working on polishing up UI elements since the button is in the wrong place and also working on having the onclick event ONLY happen when a user clicks the background of the webpage, current load happens when user clicks and element on the web page.

--- final/imageFunctions.js
+++ final/imageFunctions.js
@@ -1,50 +1,63 @@
+var offsetActual = 0;
+
 function getImages(offsetActual) {
-    var data = { "offset": offsetActual, "search": "random" };
-    var fd = new FormData();
-    //very simply, doesn't handle complete objects
-    for (var i in data) {
-        fd.append(i, data[i]);
-    }
-    fetch('http://localhost/server_side.php', {
-        method: "POST",
-        body: fd
-    })
-        .then(response => {
-            response.text()
-                .then(res => {
-                    var returnFormatted = JSON.parse(res);
-                    for (var i = 0; i < returnFormatted.length; i++) {
-                        var iframe = document.createElement('iframe');
-                        iframe.src = returnFormatted[i];
-                        document.body.appendChild(iframe);
-                    }
-                })
-        })
-
-};
-
-function getSearchImages(offsetActual) {
     var searchData = document.getElementById('search').value;
+
+    if (searchData == null || searchData == "") {
+        searchData = "random";
+    }
+
     var data = { "offset": offsetActual, "search": searchData };
     var fd = new FormData();
-    //very simply, doesn't handle complete objects
     for (var i in data) {
         fd.append(i, data[i]);
     }
     fetch('http://localhost/server_side.php', {
         method: "POST",
         body: fd
+    }).then(response => {
+        response.text()
+            .then(res => {
+                var returnFormatted = JSON.parse(res);
+                for (var i = 0; i < returnFormatted.length; i++) {
+                    var iframe = document.createElement('iframe');
+                    resizeIFrameToFitContent(iframe);
+                    iframe.setAttribute('id', 'gifFrame');
+                    iframe.src = returnFormatted[i];
+                    document.body.appendChild(iframe);
+                }
+            })
     })
-        .then(response => {
-            response.text()
-                .then(res => {
-                    var returnFormatted = JSON.parse(res);
-                    for (var i = 0; i < returnFormatted.length; i++) {
-                        var iframe = document.createElement('iframe');
-                        iframe.src = returnFormatted[i];
-                        document.body.appendChild(iframe);
-                    }
-                })
-        })
+};
 
+document.getElementById("searchButton").addEventListener("click", function () {
+    offsetActual = 0;
+    if (document.getElementById("gifFrame")) {
+        var iframes = document.querySelectorAll('iframe');
+        for (var i = 0; i < iframes.length; i++) {
+            iframes[i].parentNode.removeChild(iframes[i]);
+        }
+    }
+    getImages(offsetActual);
+});
+
+document.body.addEventListener('click', function () {
+    offsetActual += 10;
+    getImages(offsetActual);
+}, false);
+
+document.addEventListener("DOMContentLoaded", function () {
+    getImages(offsetActual);
+});
+
+function resizeIFrameToFitContent(iFrame) {
+    iFrame.height = "300";
+    iFrame.width = "480";
 }
+
+window.onscroll = function () {
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+        offsetActual += 10;
+        getImages(offsetActual);
+    }
+};
