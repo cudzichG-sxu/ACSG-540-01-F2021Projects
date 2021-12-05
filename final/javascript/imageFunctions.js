1020be2cfc2739f07b208c6a229423625da7136c
Greg <gregcudzich@gmail.com>
2021-12-05 13:33:48 -0600

    finalized web project, fixed button UI issues, fixed clearing of input field in page refresh, added JS and CSS directories to put css and js files respectively.

--- final/javascript/imageFunctions.js
+++ final/javascript/imageFunctions.js
@@ -1,18 +1,23 @@
 var offsetActual = 0;
-
+//function go get images with offset as a parameter
 function getImages(offsetActual) {
+    //grabs seach field value and below checks if it's null, if so then it's set to random
+    //mainly used for page refresh or on initial load
     var searchData = document.getElementById('search').value;
 
     if (searchData == null || searchData == "") {
         searchData = "random";
     }
 
+    //data to send to php server side
     var data = { "offset": offsetActual, "search": searchData };
     var fd = new FormData();
     for (var i in data) {
         fd.append(i, data[i]);
     }
-    fetch('http://localhost/server_side.php', {
+    //post data to php server side, on return loops through the 
+    //return urls and adds them into iFrames
+    fetch('http://acsg540.com/~gcudzich/final/server_side.php', {
         method: "POST",
         body: fd
     }).then(response => {
@@ -30,7 +35,10 @@ function getImages(offsetActual) {
     })
 };
 
-document.getElementById("searchButton").addEventListener("click", function () {
+//function called by the button when searching new items
+//resets offset to 0, removes all previously added iFrames so that 
+//it's reset to the initial 10 for new images based on user search
+function getNewImages() {
     offsetActual = 0;
     if (document.getElementById("gifFrame")) {
         var iframes = document.querySelectorAll('iframe');
@@ -39,22 +47,29 @@ document.getElementById("searchButton").addEventListener("click", function () {
         }
     }
     getImages(offsetActual);
-});
+}
 
+//event listener to add more images when the body is clicked
 document.body.addEventListener('click', function () {
     offsetActual += 10;
     getImages(offsetActual);
 }, false);
 
+//listener to load images on initial page load
 document.addEventListener("DOMContentLoaded", function () {
     getImages(offsetActual);
 });
 
+//resizes iFrames to a reasonable size
+//also combats the scroll to bottom stuff since the original size it would call
+//the API twice since on initial load it was already technically at the bottom
 function resizeIFrameToFitContent(iFrame) {
     iFrame.height = "300";
     iFrame.width = "480";
 }
 
+//scroll listener to load more images when the user
+//hits the bottom of the page, *pagination
 window.onscroll = function () {
     if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
         offsetActual += 10;
